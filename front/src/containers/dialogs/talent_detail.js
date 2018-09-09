75642c822b0df561c2d25380513d9009bdf2f151
shimizu.kenji <shimizu.kenji@noncore.net>
2018-09-09 11:57:49 +0900

    タレント詳細ダイアログを選択したタレントの内容で表示するように対応

---
+++ front/src/containers/dialogs/talent_detail.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import actions from '../../actions/dialogs';
+import TalentDetail from '../../components/dialogs/talent_detail';
+import _ from 'lodash';
+
+const mapStateToProps = state => ({
+  talent: state.talents.talents[_.findIndex(state.talents.talents, {id: state.talents.selected_talent})] || {}
+});
+
+const mapDispatchToProps = dispatch => ({
+  openDialog: (name, open) => {
+    dispatch(actions.open(name, open));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TalentDetail);
