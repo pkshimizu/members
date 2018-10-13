40534cf3501d3f4fb26a5646a8c15a3b4ea873e7
shimizu.kenji <shimizu.kenji@noncore.net>
2018-10-13 15:17:24 +0900

    debugをtrueに戻し、allow_hostを設定

--- members/settings.py
+++ members/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@&9kwu_o=!3e(+)om9(!c1=*c4fl1)&ai=pjtd4cmejj)7=bs8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.250.2.62', 'localhost']
 
 
 # Application definition
