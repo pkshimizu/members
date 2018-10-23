249d678aa20b1b02f6af8ec87599e65fbfb4ef1b
shimizu.kenji <shimizu.kenji@noncore.net>
2018-10-23 14:04:20 +0900

    管理画面でタレントの自己紹介文をテキストエリアで入力できるように対応

--- talents/admin.py
+++ talents/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from django import forms
+
 from talents.models import Talent, Floor, Seat, EmploymentStatus
 
+
+class TalentForm(forms.ModelForm):
+    self_introduction = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = Talent
+        fields = '__all__'
+
+
+class TalentAdmin(admin.ModelAdmin):
+    form = TalentForm
+
+
 admin.site.register(EmploymentStatus)
-admin.site.register(Talent)
+admin.site.register(Talent, TalentAdmin)
 admin.site.register(Floor)
 admin.site.register(Seat)
